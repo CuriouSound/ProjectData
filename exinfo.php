83773469fff666305e5107970a2b498f2bd15c71
emilieq <emiliequinonero@gmail.com>
2019-01-29 11:46:14 +0100

    fichiers de base

--- exinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'projetData';
-$user = 'ex';
-$pass = 'ex';
-
-// Créer un fichier info.php en local, spécifier login et mdp 
-// puis ajouter en gitignore dans votre dossier
