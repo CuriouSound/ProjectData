fc55f47ec5cda9be47e19d3e5a611c1713ecd63d
emilieq <emiliequinonero@gmail.com>
2019-02-04 09:16:37 +0100

    assemblage géolocalisation + index

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
-
     
 </head>
 
@@ -22,12 +21,18 @@
 <section class="container">
   <div class="row justify-content-center">
     <div class="card col-md-4">
+      <p>
+        Playlist des nouvelles sorties
+      </p>
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
     <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
         </div>
     </div>
     <div class="card col-md-4 offset-md-1 ">
+      <p>
+        Les 50 titres les plus écoutés dans le pays
+      </p>
       <img src="..." class="card-img-top" alt="...">
       <div class="card-body">
     <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
@@ -36,16 +41,27 @@
 </div>
 </section>
 
-<section class="container">
-    <div class="row">
-        <p class="col-md-12">Le top des playlists écoutées (dans le pays où l'utilisateur se trouve)</p>
-    </div>
-</section>
 
 <section id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <section class="container">
+      <div class="row justify-content-center">
+        <script>
+
+          fetch('https://translation.googleapis.com/language/translate/v2&')
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+          });
+          let url = "";
+
+        </script>
+          <p id="reponse" class="col-md-12">Le top des playlists écoutées (dans le pays où l'utilisateur se trouve)</p>
+      </div>
+  </section>
   <div class="carousel-inner">
     <div class="carousel-item active">
-    <div id="plans" class="carousel">
         <div class="carousel__container">
           <div class="car__item">
             <h1>9</h1>
@@ -64,7 +80,6 @@
             <p>Master plan</p>
           </div>
         </div>
-    </div>
     </div>
     <div class="carousel-item">
     <div class="carousel__container">
@@ -118,45 +133,26 @@
 </section>
 
 
+<!-- code Robin -->
+<!-- 
+<div ></div>
+        <div id="demo"></div>
+        
+        <p id="demo"></p> -->
 
 
+<!-- script jquery Robin -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-<script src="script.js"></script>
-
-
-<!-- Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Bootstrap
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+<!-- script -->
+<script src="script.js"></script>
 </body>
 <footer>
 
 </footer>
 </html>
-
-
-
-
-
-<?php
-
-// //Si on a une info dans $_GET['page'], c'est la page demandée
-// if(isset($_GET['page']) && !empty($_GET['page'])) {
-//     $page = $_GET['page'];
-// }
-// else {
-//     $page = 'Home'; //Si il n'y a rien dans $_GET['page'], on va sur la home
-// }
-
-// $page = ucfirst($page); //On passe la première lettre en majuscule car les controlleurs s'appellent FilmController.php et HomeController.php mais les URLs sont en général en minuscule
-
-// //On inclut le controller en fonction de la page demandée
-// if (is_file('Controllers/'.$page.'Controller.php')){
-//     include ('Controllers/'.$page.'Controller.php');  
-// } 
-// else {
-//      include ('404.php');  
-// }
-
-?>
-
--- script.js
+++ script.js
@@ -1,5 +1,67 @@
+// definititon de variable vide pour les rendre globale
+var longitude = "";
+var latitude =  "";
+
+function getLocation() {
+  // si le navigateur le permet on prends sa position avec son autorisation sinon on affiche un message pas supporte
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } 
+  else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  };
+};
+
+function showPosition(position) {
+
+  // sert a afficher longitude et latitude
+  // var x = document.getElementById("demo");
+  // x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+
+  longitude = position.coords.longitude;
+  latitude = position.coords.latitude; 
+   
+  // requete a l'api en demandant la pos on concatene les variables ; on spécifie à la fin le langage de la réponse ; ça permet d'avoir le nom du pays toujours en Anglais, pour que Spotify fasse la bonne recherche.
+  var lon="lon="+longitude;
+  var lat="lat="+latitude;
+
+  //dupliquer cette requete pour avoir le nom en Français
+  var url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&"+ lon + '&' + lat +"&"+"accept-language=en";
+
+    $.ajax({
+        url: url,
+        
+    })
+    .done(function( response ) {
+      // le nom du pays se situe dans la case address, on l'affiche dans #response
+      let paysreponse = response.address.country;
+
+      // On peut ensuite utiliser la variable paysreponse comme élément de la requête à l'API spotify
+
+    });
+    
+    var urlfr = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&"+ lon + '&' + lat +"&"+"accept-language=fr";
+    $.ajax({
+      url: urlfr,
+    })
+    .done(function( response ) {
+      // sert a afficher le contenu du pays
+      var divReponse = document.getElementById('reponse');
+      // le nom du pays se situe dans la case address, on l'affiche dans #response
+      let paysreponsefr = response.address.country;
+      
+      divReponse.innerHTML = "Le top des playlists écoutées dans le pays : "+paysreponsefr+".";
+    });
+}
+
+
+//Ces fonctions s'effectuent quand la page est chargée
 $('document').ready(function(){
+
+    //appel de la fonction qui géolocalise
+    getLocation();
   
+    //code du carousel
     var sldsToShow = 3;
     
     if (parseInt($(window).width()) < 700) {
@@ -11,9 +73,12 @@ $('document').ready(function(){
       slidesToShow: sldsToShow,
       prevArrow: "<img src='https://raw.githubusercontent.com/rnas/frontend-test/master/assets/caret_left.png' data-role='none' class='slick-prev'>",
       nextArrow: "<img src='https://raw.githubusercontent.com/rnas/frontend-test/master/assets/caret_right.png' data-role='none' class='slick-next'>"
-    })
+    });
       
     $('.mob-menu').click(function(){
       $('.dropdown').toggle();
-    })
-  });
+    });
+
+
+
+});
